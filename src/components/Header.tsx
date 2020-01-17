ec8a7ebd1e16131a12396f6e5c41513d150a9451
RanolP <public.ranolp@gmail.com>
2020-01-17 23:50:55 +0900

    v1.6.0

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -27,7 +27,7 @@ const Link = styled.a`
 const Header: FC = () => {
   return (
     <Wrap>
-      <span>파랑새밈 v1.5.1</span>
+      <span>파랑새밈 v1.6.0</span>
       <span>
         <Link href="https://twitter.com/Ranol__">Ranol☆P</Link>가 만들었어요!
       </span>
