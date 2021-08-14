9154626c3f3798d740c90a78c4e15c14e2c7565a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-08-14 23:04:31 +0000

    Deploy RanolP/bluebird-meme to github.com/RanolP/bluebird-meme.git:gh-pages

---
+++ precache-manifest.71d7fce2c3779659c17d59070acafe61.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74deac1b40081084cdd659bbbb861dc7",
+    "url": "/bluebird-meme/index.html"
+  },
+  {
+    "revision": "e32fd9f686373258dbf3",
+    "url": "/bluebird-meme/static/js/2.b6b160b2.chunk.js"
+  },
+  {
+    "revision": "ca9cc1c18c3e84e3081a2a083218c4e2",
+    "url": "/bluebird-meme/static/js/2.b6b160b2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0433849cb48830a084b3",
+    "url": "/bluebird-meme/static/js/main.0d8a3cae.chunk.js"
+  },
+  {
+    "revision": "c973c403f199cac28c14",
+    "url": "/bluebird-meme/static/js/runtime-main.f531119d.js"
+  }
+]);
